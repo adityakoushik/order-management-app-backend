e1c722c91041be373985a12940a8c32cfe7c5c09
Koushik Dutta <duttakoushik760@gmail.com>
2025-09-10 21:50:08 +0530

    Add logging for broadcast channel authorization to improve traceability

--- laravel/routes/channels.php
+++ laravel/routes/channels.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('orders.{customerId}', function ($user, $customerId) {
+	\Log::info('Broadcast auth', ['user_id' => $user->id, 'customerId' => $customerId]);
 	return (int) $user->id === (int) $customerId;
 });
