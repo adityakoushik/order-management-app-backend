e4c91d760de83c4a7ffeade54035007bcb44d99e
Koushik Dutta <duttakoushik760@gmail.com>
2025-09-10 21:37:56 +0530

    Integrate Pusher and Laravel Echo for real-time event broadcasting

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
 	->withRouting(
 		web: __DIR__ . '/../routes/web.php',
 		api: __DIR__ . '/../routes/api.php', // <--- This line is required!
+		channels: __DIR__ . '/../routes/channels.php',
 		commands: __DIR__ . '/../routes/console.php',
 		health: '/up',
 	)
